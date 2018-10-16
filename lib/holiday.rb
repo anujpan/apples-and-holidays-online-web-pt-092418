d32120b1d4a8855594c15771413ef616463a90b8
Anuj Panchal <anujpan1@gmail.com>
2018-10-16 21:04:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season}"
     
-    holiday.each do |
+    holiday.each do |holiday, supply|
+      
+    end
   end
 end
 
